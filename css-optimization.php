0a06859646a39f16a48e7a0c618a5209a716778d
optimalisatie <info@optimalisatie.nl>
2018-02-18 18:52:53 +0100

    0.0.1 beta

--- css-optimization.php
+++ css-optimization.php
@@ -30,7 +30,7 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 
 // settings
 $module_version = '0.0.1';
-$minimum_core_version = '0.0.2';
+$minimum_core_version = '0.0.3';
 $plugin_path = dirname(__FILE__);
 
 // load the optimization module loader
