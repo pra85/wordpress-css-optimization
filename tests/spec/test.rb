e3e73042a83a9e98ac966303a9382738677b4066
optimalisatie <info@optimalisatie.nl>
2018-03-08 09:29:38 +0100

    tests

--- tests/spec/test.rb
+++ tests/spec/test.rb
@@ -157,7 +157,7 @@ describe "wordpress: #{uri}/ - ", :type => :request, :js => true do
 
       # delete
       accept_confirm do
-        find("tr[data-slug='css-optimization']").find('td.column-primary').find(".delete", match: :first).find("a").click
+        find("tr[data-slug='css-optimization']").find('td.column-primary').find("a.delete").click
       end
 
       expect(page).to have_content("was successfully deleted.");
